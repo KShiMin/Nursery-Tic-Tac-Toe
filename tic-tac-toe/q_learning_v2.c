670b8e7d3133f8577af3d0578dc91b5111b58a8e
Shi Min <shiminlove20@gmail.com>
2024-11-19 00:26:56 +0800

    refactor(q_learning_v2.c): added more key functions for training q-learn
    train is about 80% done. Added updateBoardState and addState to update
    tic-tac-toe board and add state to player. Added check_win to check
    if there is a win or draw, else contiune the game.

--- tic-tac-toe/q_learning_v2.c
+++ tic-tac-toe/q_learning_v2.c
@@ -2,13 +2,20 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define MAX_STRINGS 100
-#define MAX_LENGTH 100
-#define QTABLE_LENGTH 1000
+#define MAX_STRINGS 100 // num of string array can hold
+#define MAX_LENGTH 9 // how long of a string it can hold
+#define QTABLE_LENGTH 5000
 
 
-const int human = 1, cpu = -1, blank = 0;
 const float lr=0.2, decay =0.9; //Q-learning parameters
+
+
+// Player type integer definition
+typedef enum {
+    BLANK = 0,
+    HUMAN = 1,
+    CPU = -1
+} PlayerType;
 
 // defining own boolean datatye to avoid confusion
 typedef enum{
@@ -36,7 +43,7 @@ typedef struct{
 // Simulate AI
 typedef struct{
     
-    char state[MAX_STRINGS][MAX_LENGTH]; // hold all state movements
+    int state[MAX_STRINGS][MAX_LENGTH]; // hold all state movements
     Qvalue *state_val[QTABLE_LENGTH]; // Array of q-values (Q-Table)
     float exp_rate; 
 
@@ -49,25 +56,44 @@ typedef struct{
     int playing; // starting player
 } Game;
 
+// Initialise Player object
+void initPlayer(Player *player, float exp_rate){
+    printf("Initialising Player...\n");
+    // initalise empty state array
+    for (int i=0 ; i < MAX_STRINGS; i++){
+        player->state[i][0] = '\0'; // setting states to 0
+    }
+
+    // initalise Q-Table with 0
+    for(int i=0; i < QTABLE_LENGTH; i++){
+        player->state_val[i] = malloc(sizeof(Qvalue)); // allocate memory for each Q-value
+    }
+
+    // set exploration rate
+    player->exp_rate = exp_rate;
+    printf("Player Initialised\n");
+}
+
+
 // Convert char 2D array to int 2D array for qlearning to understand
 char convertBoard(char board[3][3], int convertedState[3][3]){
-    
+    printf("Converting Board...\n");
     for(int i=0; i< 3; i++){
         for(int j=0; j<3; j++){
             switch (board[i][j]){
                 case 'X':
-                    convertedState[i][j] = cpu;
+                    convertedState[i][j] = CPU;
                     break;
                 case 'O':
-                    convertedState[i][j] = human;
+                    convertedState[i][j] = HUMAN;
                     break;
                 case '-':
-                    convertedState[i][j] = blank;       
+                    convertedState[i][j] = BLANK;       
                     break;
             }
         }
     }
-
+    printf("Board Converted\n");
 }
 
 void printConvertedBoard(int convertedState[3][3]) {
@@ -84,12 +110,15 @@ void printConvertedBoard(int convertedState[3][3]) {
 void boardToString(int board[3][3], int results[9]){
     int index = 0;
 
+    printf("Converting Board to 1d array...\n");
+
     for(int i = 0; i<3; i++){
         for(int j = 0; j<3; j++){
             results[index] = board[i][j];
             index++;
         }
     }
+    printf("Board Converted\n");
 }
 
 void copyBoard(int board[3][3], int nextBoard[3][3]){
@@ -115,6 +144,8 @@ int availPos(int board[3][3], Coord availCoord[9]){
     
     int index = 0;
 
+    printf("Getting Available Position...\n");
+
     for(int i = 0; i<3; i++){
         for(int j = 0; j<3; j++){
             if (board[i][j] == 0){
@@ -124,6 +155,9 @@ int availPos(int board[3][3], Coord availCoord[9]){
             }
         }
     }
+
+    printf("Available Position Gotten.\n");
+
     return index;
 }
 
@@ -172,11 +206,86 @@ Coord playerAction(Coord position[], int pos_index,int board[3][3], int playerSy
     return action;
 
 }
+
+// Update Board State
+void updateBoardState(int board[3][3], Coord action, Game game){
+    printf("Updating Board...\n");
+    board[action.row][action.col] = game.playing;
+    game.playing = -game.playing; // switch player
+    printf("Board Updated\n");
+}
+
+// Add state to current player
+void addState(Player* p, int board1d[9]){
+    for(int i=0; i<MAX_STRINGS; i++){
+        if(p->state[i] == '\0'){
+            for(int j=0; j<9; j++){
+                p->state[i][j] = board1d[j]; //copy 1d array to state
+            }
+            break;
+        }
+    }
+}
+
+
+int check_win(int board[3][3], Game* game){
+    int diag_sum1 = 0, diag_sum2 = 0;
+
+    // Check rows and columns
+    for(int i=0; i<3;i++){
+        int row_sum = 0, col_sum = 0;
+
+        for(int j=0; j<3; j++){
+            row_sum += board[i][j]; //Sum the row
+            col_sum += board[j][i]; //Sum the column
+        }
+
+        if (abs(row_sum) == 3){
+            game->game_status = true;
+            return row_sum > 0 ? 1: -1;
+        }
+
+        if (abs(col_sum) == 3){
+            game->game_status = true;
+            return col_sum > 0? 1: -1;
+        }
+
+        // Check Diagonal
+        diag_sum1 += board[i][i]; //Top-Left to Bottom-Right
+        diag_sum2 += board[i][3-i-1]; // Top-Right to Bottom-Left
+    }
+
+    if (abs(diag_sum1) == 3){
+        game->game_status = true;
+        return diag_sum1 > 0 ? 1: -1;
+    }
+
+    if (abs(diag_sum2) == 3){
+        game->game_status = true;
+        return diag_sum2 > 0 ? 1: -1;
+    }
+
+    Coord avail_pos[9];
+    int pos_index = availPos(board, avail_pos);
+    if(pos_index == 0){
+        game->game_status = true;
+        return 0; // if no winner, game is tie
+    }
+
+    return -99; // Game contiunes
+}
+
 // paras: board --> convertedBoard, episode - amt of game rounds
 // Training qlearning
-void train(int board[3][3], int episode){
+void train(int episode){
     
+    int board[3][3] = {0};
     Player players[2];
+
+    // Initialise Players
+    for(int p=0; p<2; p++){
+        initPlayer(&players[p],0.3);
+    }
 
     for (int i=0; i<episode; i++){
         printf("Training Round %d\n", i);
@@ -188,9 +297,22 @@ void train(int board[3][3], int episode){
         // 
         while(!game.game_status){   // while game not end
             for(int p=0; p<2; p++){
+                int board1d[9];
                 Coord avail_pos[9];
                 int pos_index = availPos(board, avail_pos);
+                Coord action = playerAction(avail_pos, pos_index, board, game.playing, players[p]);
+                
+                updateBoardState(board, action, game);
+                
+                boardToString(board, board1d);
 
+                addState(&players[p],board1d);
+
+                int win = check_win(board, &game);
+                if(!win == -99){
+
+                };
+                
             }
         }   
 
@@ -225,11 +347,9 @@ int main(){
     // }
     printf("Test Index is %d\n", test_index);
 
-    Player p1 = {
-        state: {},
-        state_val: {},
-        exp_rate: 0.3
-    };
+    Player p1;
+    initPlayer(&p1, 0.3);
+
     Coord pos = playerAction(test, test_index,convertedState, -1, p1);
     printf("Row: %d, Column: %d\n", pos.row, pos.col);
 
